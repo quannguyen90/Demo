82fd8b7152c5b55360bb31af56d3693cac6ceca6
Quan Nguyen Van <quannv.tm@gmail.com>
2015-11-26 14:26:15 +0700

    initial code

---
+++ Example/Pods/Target Support Files/Pods-Demo_Example/Pods-Demo_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Demo_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Demo_ExampleVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-Demo_Tests/Pods-Demo_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Demo_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Demo_TestsVersionString[];
+
